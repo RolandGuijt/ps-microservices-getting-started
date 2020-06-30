3da23376095e5547c42e7e93b3b1699669199f93
Gill Cleeren <gill@snowball.be>
2020-06-30 13:57:54 +0200

    EventCatalog

---
+++ GloboTicket/GloboTicket.Services.TicketCatalog/DbContexts/EventCatalogDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GloboTicket.Services.EventCatalog.DbContexts
+{
+    public class EventCatalogDbContext
+    {
+    }
+}
--- GloboTicket/GloboTicket.Services.TicketCatalog/Entities/Event.cs
+++ GloboTicket/GloboTicket.Services.TicketCatalog/Entities/Event.cs
@@ -9,7 +9,6 @@ namespace GloboTicket.Services.EventCatalog.Entities
         public int Price { get; set; }
         public string Artist { get; set; }
         public DateTime Date { get; set; }
-        public DateTime LastModifiedDate { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
         public int CategoryId { get; set; }
