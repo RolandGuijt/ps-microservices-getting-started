b264fad7917aa12b5c524d9b163c0d00661b9c9c
Kevin Dockx <kevin.dockx@gmail.com>
2020-06-30 16:03:37 +0200

    Basket api

---
+++ GloboTicket/GloboTicket.Services.ShoppingBasket/Profiles/BasketLineProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GloboTicket.Services.ShoppingBasket.Profiles
+{
+    public class BasketLineProfile : Profile
+    {
+        public BasketLineProfile()
+        {
+            CreateMap<Models.BasketLineForCreation, Entities.BasketLine>();
+            CreateMap<Models.BasketLineForUpdate, Entities.BasketLine>();
+            CreateMap<Entities.BasketLine, Models.BasketLine>().ReverseMap();
+
+        }
+    }
+}
--- GloboTicket/GloboTicket.Services.ShoppingBasket/Repositories/IBasketRepository.cs
+++ GloboTicket/GloboTicket.Services.ShoppingBasket/Repositories/IBasketRepository.cs
@@ -6,8 +6,12 @@ namespace GloboTicket.Services.ShoppingBasket.Repositories
 {
     public interface IBasketRepository
     {
+        Task<bool> BasketExists(Guid basketId);
+
         Task<Basket> GetBasketById(Guid basketId);
 
         void AddBasket(Basket basket);
+
+        Task<bool> SaveChanges();
     }
 }
