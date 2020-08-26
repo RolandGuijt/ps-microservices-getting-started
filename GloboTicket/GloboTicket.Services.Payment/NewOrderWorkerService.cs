c19d1db51783cf5e76185fbc71ac6f0846614dc3
Roland Guijt <roland.guijt@gmail.com>
2020-08-26 16:03:22 +0200

    Payment service now uses work service

---
+++ GloboTicket/GloboTicket.Services.Payment/NewOrderWorkerService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Storage;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Rebus.Activation;
+using Rebus.Config;
+
+namespace GloboTicket.Services.Payment
+{
+    public class NewOrderWorkerService: BackgroundService
+    {
+        private readonly ILogger<NewOrderWorkerService> _logger;
+
+        public NewOrderWorkerService(ILogger<NewOrderWorkerService> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            var storageAccount = CloudStorageAccount.Parse(config["AzureQueues:ConnectionString"]);
+
+            using var activator = new BuiltinHandlerActivator();
+            activator.Register(() => new NewOrderHandler());
+            Configure.With(activator)
+                .Transport(t => t.UseAzureStorageQueues(storageAccount, config["AzureQueues:QueueName"]))
+                .Start();
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+            }
+        }
+    }
+}
--- GloboTicket/GloboTicket.Services.Payment/Program.cs
+++ GloboTicket/GloboTicket.Services.Payment/Program.cs
@@ -1,28 +1,25 @@
 using System;
-using Microsoft.Azure.Storage;
-using Microsoft.Extensions.Configuration;
-using Rebus.Activation;
-using Rebus.Config;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace GloboTicket.Services.Payment
 {
     public class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-
-            var storageAccount = CloudStorageAccount.Parse(config["AzureQueues:ConnectionString"]);
-
-            using var activator = new BuiltinHandlerActivator();
-            activator.Register(() => new NewOrderHandler());
-            Configure.With(activator)
-                .Transport(t => t.UseAzureStorageQueues(storageAccount, config["AzureQueues:QueueName"]))
-                .Start();
-
+            CreateHostBuilder(args).Build().Run();
             Console.WriteLine("Listening for new orders..");
             Console.WriteLine("Press enter to quit");
             Console.ReadLine();
         }
+
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<NewOrderWorkerService>();
+                });
     }
 }
