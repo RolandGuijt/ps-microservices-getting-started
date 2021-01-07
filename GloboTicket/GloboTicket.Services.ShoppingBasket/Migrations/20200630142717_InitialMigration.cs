4cda2fab705ab84bc530e82d9090e1b8ab37ee7c
Roland Guijt <roland.guijt@gmail.com>
2021-01-07 10:17:22 +0100

    Delete seed data shoppingbasket

--- GloboTicket/GloboTicket.Services.ShoppingBasket/Migrations/20200630142717_InitialMigration.cs
+++ GloboTicket/GloboTicket.Services.ShoppingBasket/Migrations/20200630142717_InitialMigration.cs
@@ -39,21 +39,6 @@ namespace GloboTicket.Services.ShoppingBasket.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
-            migrationBuilder.InsertData(
-                table: "Baskets",
-                columns: new[] { "BasketId", "UserId" },
-                values: new object[] { new Guid("d28888e9-2ba9-473a-a40f-e38cb54f9b35"), new Guid("6c9fe94e-257a-42e2-a49c-1b216d4e81be") });
-
-            migrationBuilder.InsertData(
-                table: "BasketLines",
-                columns: new[] { "BasketLineId", "BasketId", "EventId", "TicketAmount" },
-                values: new object[] { new Guid("75918bea-7a04-406e-bafd-51dc8b98816f"), new Guid("d28888e9-2ba9-473a-a40f-e38cb54f9b35"), new Guid("e29f3df4-d9b4-4182-84dc-4289ac17c0c0"), 3 });
-
-            migrationBuilder.InsertData(
-                table: "BasketLines",
-                columns: new[] { "BasketLineId", "BasketId", "EventId", "TicketAmount" },
-                values: new object[] { new Guid("bec71e6b-6b3d-444e-85d7-77bdb3988908"), new Guid("d28888e9-2ba9-473a-a40f-e38cb54f9b35"), new Guid("39144996-8bad-4cb8-9029-125d88808377"), 2 });
-
             migrationBuilder.CreateIndex(
                 name: "IX_BasketLines_BasketId",
                 table: "BasketLines",
